7b6ef83e8759cb3947632570b311cde59e00d8a1
lesmacar31 <lcastillor4@miumg.edu.gt>
2021-11-02 21:57:18 -0600

    Cambios de vistas

--- resources/views/orden_trabajo/index.blade.php
+++ resources/views/orden_trabajo/index.blade.php
@@ -13,18 +13,33 @@
         <thead>
         <tr>
             <th scope="col">No.</th>
-            <th scope="col">Fecha de Recepcion</th>
-            <th scope="col">Acciones</th>
+            <th scope="col">Fecha de Recepción</th>
+            <th scope="col">Nit</th>
+            <th scope="col">Nombre del Cliente</th>
+            <th scope="col">Placa</th>
+            <th scope="col">Total</th>
+            <th scope="col">Estado</th>
+            <th scope="col" class="text-center">Vista Previa</th>
         </tr>
         </thead>
         <tbody>
         @foreach($OrdenesTrabajos as $OrdenTrabajo)
             <tr>
                 <th scope="row">{{$OrdenTrabajo->id}}</th>
-                <td>{{$OrdenTrabajo->cleintes_id}}</td>
-                <td>{{$OrdenTrabajo->vehiculos_id}}</td>
                 <td>{{$OrdenTrabajo->fecha_recepcion}}</td>
+                <td>{{$OrdenTrabajo->clientes_id}}</td>
+                <td>{{$OrdenTrabajo->vehiculos_id}}</td>
                 <td>{{$OrdenTrabajo->users_id}}</td>
+                <td>{{$OrdenTrabajo->users_id}}</td>
+                <td>{{$OrdenTrabajo->users_id}}</td>
+                <td class="text-center"> <form>
+                        {{--                        @csrf @method('DELETE')--}}
+                        <a href="{{route('orden_trabajo.index')}}" class="btn btn-outline-info btn-sm ">
+                            <i class=" far fa-eye"> </i>
+                            VER</a>
+                    </form>
+
+                </td>
             </tr>
         @endforeach
         </tbody>
